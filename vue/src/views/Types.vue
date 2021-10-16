212a6355a7b159d7e0091fa829d2331982cc7d93
Joe S <schnetzlerjoe@gmail.com>
2021-10-15 23:17:58 -0500

    Init

--- vue/src/views/Types.vue
+++ vue/src/views/Types.vue
@@ -2,6 +2,9 @@
 	<div>
 		<div class="container">
 			<!-- this line is used by starport scaffolding # 4 -->
+		<SpType modulePath="schnetzlerjoe.planet.blog" moduleType="TimedoutPost"  />
+		<SpType modulePath="schnetzlerjoe.planet.blog" moduleType="SentPost"  />
+		<SpType modulePath="schnetzlerjoe.planet.blog" moduleType="Post"  />
 		</div>
 	</div>
 </template>
